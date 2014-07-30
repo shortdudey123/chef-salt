79d31e83fc662de7ddd3ce342a0d2652b2d9b646
Daryl Robbins <daryl@robbins.name>
2014-07-30 09:03:53 -0400

    Improve integration testing across supported platforms

---
+++ test/integration/default/serverspec/salt_spec.rb
@@ -0,0 +1,77 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.path = '/usr/bin'
+  end
+end
+
+describe 'Salt Key Exchange' do
+
+  describe command('salt-key --list=pre') do
+    its(:stdout) { should match /default-/ }
+  end
+
+end
+
+describe 'Salt Master' do
+
+  describe package('salt-master') do
+    it { should be_installed }
+  end
+
+  # Service check not supported in RedHat 5, however we still have the proccess check
+  unless (os[:family] == 'RedHat' and os[:release] =~ /^5\./)
+    describe service('salt-master') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+
+  describe process("salt-master") do
+    it { should be_running }
+  end
+
+  describe file('/etc/salt/master') do
+    it { should be_file }
+    it { should contain 'interface: 0.0.0.0' }
+  end
+
+  describe port(4505) do
+    it { should be_listening.with('tcp') }
+  end
+
+  describe port(4506) do
+    it { should be_listening.with('tcp') }
+  end
+
+end
+
+describe 'Salt Minion' do
+
+  describe package('salt-minion') do
+    it { should be_installed }
+  end
+
+  # Service check not supported in RedHat 5, however we still have the proccess check
+  unless (os[:family] == 'RedHat' and os[:release] =~ /^5\./)
+    describe service('salt-minion') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+
+  describe process("salt-minion") do
+    it { should be_running }
+  end
+
+  describe file('/etc/salt/minion') do
+    it { should be_file }
+    it { should contain 'master: localhost' }
+    its(:content) { should match /id: default-/ }
+  end
+
+end
