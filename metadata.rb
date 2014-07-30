9a36f8394254536fc9a564626c4c8cdbcdd5e61e
Daryl Robbins <daryl@robbins.name>
2014-07-30 09:18:24 -0400

    Messed up on RHEL family dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'daryl@robbins.name'
 license           'Apache 2.0'
 description       'Installs and configures Salt'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe 'master', 'Installs and configures a Salt master'
 recipe 'minion', 'Installs and configures a Salt minion'
@@ -12,7 +12,7 @@ recipe 'minion', 'Installs and configures a Salt minion'
 supports 'ubuntu', '>= 10.04'
 
 %w(redhat centos scientific amazon oracle).each do |os|
-  supports 'redhat', '>= 5.0'
+  supports os, '>= 5.0'
 end
 
 depends 'apt',              '~> 2.3.10'
