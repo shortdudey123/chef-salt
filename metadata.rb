b7725db70a2c503f6808b5c487b308a8282397af
Daryl Robbins <daryl@robbins.name>
2014-07-31 20:52:17 -0400

    Forgot to update metadata.rb for new supported OSes

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,14 @@ maintainer_email  'daryl@robbins.name'
 license           'Apache 2.0'
 description       'Installs and configures Salt'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 recipe 'master', 'Installs and configures a Salt master'
 recipe 'minion', 'Installs and configures a Salt minion'
 
 supports 'ubuntu', '>= 10.04'
+supports 'fedora', '>= 19'
+supports 'debian', '~> 7.0'
 
 %w(redhat centos scientific amazon oracle).each do |os|
   supports os, '>= 5.0'
