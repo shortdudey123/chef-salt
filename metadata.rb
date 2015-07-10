52c76e74b16cefc6f8ef1cf58fe787916bcb4a5b
Maurizio Pillitu <maoo@apache.org>
2015-07-10 16:09:14 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'daryl@robbins.name'
 license           'Apache 2.0'
 description       'Installs and configures Salt'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          	'0.2.0'
+version          	'0.2.1'
 
 recipe 'salt::master', 'Installs and configures a Salt master'
 recipe 'salt::minion', 'Installs and configures a Salt minion'
